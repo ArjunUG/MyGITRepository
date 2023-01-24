566332eaadfb3be9db5ecafc3f028c3c31f27381
ArjunUG <arjun.u.govind@gmail.com>
2023-01-24 12:20:02 +0530

    My 2nd project codes

---
+++ src/test/java/com/git/MyGITDemo/GITDemo.java
@@ -0,0 +1,25 @@
+package com.git.MyGITDemo;
+
+public class GITDemo {
+
+	public void method1() {
+		System.out.println("1st method");
+
+	}
+	public void method2() {
+		System.out.println("2nd method");
+
+	}
+	public void method3() {
+		System.out.println("3rd method");
+
+	}
+	public void method4() {
+		System.out.println("4th method");
+
+	}
+	public void method5() {
+		System.out.println("5th method");
+
+	}
+}
